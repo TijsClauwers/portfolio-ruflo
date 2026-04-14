9fa7a6b47403ac15bcfa96c082bb9f49671ece2d
Ruflo <ruflo@ruflo.run>
2026-04-14 20:10:16 +0000

    fix: alias react for @sanity/vision useEffectEvent compatibility

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,6 +8,16 @@ const nextConfig = {
     ],
   },
   trailingSlash: true,
+  webpack: (config) => {
+    // Use project React (19.2.5) for @sanity/vision instead of Next.js bundled canary
+    // which doesn't export useEffectEvent
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
